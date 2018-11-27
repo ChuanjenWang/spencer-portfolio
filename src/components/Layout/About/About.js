bbcf3ba6cdeeb61809a69714e0ff37de0a9f7e28
Spencer.Wang <chuanjen.wang@gmail.com>
2018-11-27 14:20:50 +0800

    complete home component

--- src/components/Layout/About/About.js
+++ src/components/Layout/About/About.js
@@ -4,9 +4,7 @@ import classes from './About.module.scss';
 const About = () => (
     <section className={classes.About} id="about">
         <div className="container">    
-            <div>
-                <h1>About</h1>
-            </div>
+            
         </div>
     </section>
 )
