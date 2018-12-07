df13da71c84604ab40bce4cdb6654da9dbc2c345
Spencer.Wang <chuanjen.wang@gmail.com>
2018-12-07 14:46:23 +0800

    complete skills component RWD

--- src/components/Layout/Skills/Skills.js
+++ src/components/Layout/Skills/Skills.js
@@ -1,39 +1,33 @@
 import React from 'react';
 
 import classes from './Skills.module.scss';
-import Card from '../../Card/Card';
+import Card from '../../UI/Card/Card';
 
 const Skills = () => (
     <section className={classes.Skills} id="skills">
     <div className="container">
         <div className={classes.Skills__title}>My Skills</div>
         <div className={classes.Skills__list}>
-            <ul>
-                <li>
-                    <Card icon="frontend" title="Frontend">React, Redux, Jest, Enzyme, Javascript, jQuery, HTML, CSS, Sass, RWD design</Card>
-                </li>
-                <li>
+            <div className={classes.Row}>
+                <div>
+                    <Card icon="frontend" title="Frontend">React, Redux, Jest, Enzyme, Javascript, jQuery, HTML, CSS, Sass, RWD design </Card>  
+                </div>
+                <div>
                     <Card icon="backend" title="Backend">Node.js, Express, C#, .NET MVC, RESTful API design </Card>
-                </li>
-                <li>
+                </div> 
+                <div>
                     <Card icon="database" title="Databse">MangoDB, My SQL, SQL Sevrver, Oracle, DB2</Card>
-                </li>
-                <li>
+                </div>
+                <div>
                     <Card icon="tools" title="Tools">Git, VS Code, Sketch</Card>
-                </li>
-                <li>
+                </div>
+                <div>
                     <Card icon="media" title="Media Markering">SEO, Google analytics </Card>
-                </li>
-                <li>
+                </div>
+                <div>
                     <Card icon="project" title="Poject Management">scrum methodology, gantt chart </Card>
-                </li>
-                {/* <li>
-                    <Card title="Poject Management">NodeJs, Express, C#, .NET MVC </Card>
-                </li>
-                <li>
-                    <Card title="Poject Management">NodeJs, Express, C#, .NET MVC </Card>
-                </li> */}
-            </ul>
+                </div>
+            </div>
         </div>
         </div>
     </section>
