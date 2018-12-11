3db0c28c81838826ea9ea81d9235661a79f18fb6
Spencer.Wang <chuanjen.wang@gmail.com>
2018-12-11 16:21:54 +0800

    fixed mobile backdrop move problem

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -5,11 +5,13 @@ import classes from './Backdrop.module.scss';
 const Backdrop = (props) => 
 {
     const backdropClass = props.show ? [classes.backdrop, classes.open] : [classes.backdrop];
-    const contentClass = [classes.backdrop__content, classes.overlay];
+    //const contentClass = [classes.backdrop__content, classes.overlay];
     if(props.show) {
-        document.body.classList.add(classes.noscroll);
+        //document.body.classList.add(classes.noscroll);
+        document.body.style.overflow = 'hidden';
     }else {
-        document.body.classList.remove(classes.noscroll);
+        //document.body.classList.remove(classes.noscroll);
+        document.body.style = '';
     }
 
     return (
